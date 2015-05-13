d16652a8444f1c4392522184ec850ef6bb073c2a
A S <ahmed.sharifi@gmail.com>
2015-05-13 18:09:24 -0400

    updated header and footers and added uikit

--- header.inc.php
+++ header.inc.php
@@ -6,16 +6,18 @@ echo '
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 		<title>License Information</title>
+		<link rel="stylesheet" href="'.BASE_URL.'uikit-2.20.3/css/uikit.min.css" type="text/css" />
 		<link rel="stylesheet" href="'.BASE_URL.'stylesheets/jquery-ui.css" type="text/css" />
 		<link rel="stylesheet" href="'.BASE_URL.'stylesheets/license.css" type="text/css" />
 		<script type="text/javascript" src="'.JQUERY.'"></script>
+		<script type="text/javascript" src="'.BASE_URL.'uikit-2.20.3/js/uikit.min.js"></script>
 		<script type="text/javascript" src="'.JQUERYUI.'"></script>
 		<script type="text/javascript" src="'.BASE_URL.'scripts/jquery.infieldlabel.min.js"></script>
 		<script type="text/javascript" src="'.BASE_URL.'scripts/script.js"></script>
 		'.$moreinsert.'
 	</head>
 	<body>
-	<div style="text-align:center">
-	<a href="'.BASE_URL.'"><img src="'.BASE_URL.'/mlg.gif" alt="MONDO LICENSE GRINDER MMX" /></a>
-	</div>
+
+	<div class="uk-container uk-container-center uk-margin-top uk-margin-large-bottom">
+
 ';
