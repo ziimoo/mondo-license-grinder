1ce5d011b48100794e4358a38d652660885989c4
A S <ahmed.sharifi@gmail.com>
2015-05-13 17:23:20 -0400

    using getenv

--- config.php
+++ config.php
@@ -1,10 +1,12 @@
 <?php
 define('DBTYPE','mysql');
-define('DBHOST', $OPENSHIFT_MYSQL_DB_HOST);
-define('DBPORT', $OPENSHIFT_MYSQL_DB_PORT);
-define('DBUSER','admin8hZBFZs');
-define('DBPASS','IJkM3P1gItbg');
-define('DBNAME','mondo');
+
+define('DBHOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
+define('DBPORT', getenv('OPENSHIFT_MYSQL_DB_PORT'));
+define('DBUSER', getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
+define('DBPASS', getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
+define('DBNAME', getenv('OPENSHIFT_GEAR_NAME'));
+
 define('JQUERY','http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js');
 define('JQUERYUI','http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.5/jquery-ui.min.js');
 define('DOCSTORE','/path/to/licensedocs');
